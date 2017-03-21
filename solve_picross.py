07cef1a2242f886e3784c1ad236d678f40e75171
CJ Carey <perimosocordiae@gmail.com>
2017-03-21 11:43:10 -0400

    Adding OCR/animation support to the runner

--- solve_picross.py
+++ solve_picross.py
@@ -1,35 +1,72 @@
 #!/usr/bin/env python
+from __future__ import print_function, division
 import ast
 import numpy as np
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 from webtool import webtool, webfn, webarg
 
-from picross import solve, FULL, EMPTY
+from picross import solve, iter_solve, FULL, EMPTY, ConstraintsDetector
 
 
 def main():
   ap = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
   ap.add_argument('--port', type=int, default=8787, help='Port for web UI.')
+  ap.add_argument('--image', help='Path to screenshot of puzzle.')
+  ap.add_argument('--ocr-tpl-path', default='templates.npz',
+                  help='Path to OCR digit templates file.')
+  ap.add_argument('--animate', action='store_true',
+                  help='Use Bayesian solver to produce an animation.')
   ap.add_argument('row', nargs='?', help='Row constraints, comma-separated.')
   ap.add_argument('col', nargs='?', help='Column constraints, comma-separated.')
   args = ap.parse_args()
 
-  if args.row is None and args.col is None:
+  if args.image:
+    with ConstraintsDetector(args.ocr_tpl_path) as ocr:
+      rows, cols = ocr.detect_constraints(args.image)
+    print('Rows:', rows)
+    print('Cols:', cols)
+    return cli_main(rows, cols, pre_parsed=True, animate=args.animate)
+  elif args.row is None and args.col is None:
+    if args.animate:
+      ap.error('Cannot pass --animate to web UI.')
     return webtool(title='Picross', port=args.port, fn=web_main)
   if args.row is not None and args.col is not None:
-    return cli_main(args.row, args.col)
+    return cli_main(args.row, args.col, animate=args.animate)
   ap.error('Must supply both row and col constraints, or neither.')
 
 
-def cli_main(rows, cols):
-  rows = parse_constraint(rows)
-  cols = parse_constraint(cols)
+def cli_main(rows, cols, pre_parsed=False, animate=False):
+  if not pre_parsed:
+    rows = parse_constraint(rows)
+    cols = parse_constraint(cols)
+  if animate:
+    return animate_solution(rows, cols)
   soln = solve(rows, cols)
-  out = np.full_like(soln, ' ', dtype=bytes)
-  out[soln == EMPTY] = '_'
+  out = np.full_like(soln, '_', dtype=bytes)
+  out[soln == EMPTY] = ' '
   out[soln == FULL] = 'x'
   for row in out:
     print(''.join(row))
+
+
+def animate_solution(rows, cols, anim_secs=5, end_secs=1):
+  frames = list(iter_solve(rows, cols))
+  print('Solved in', len(frames), 'steps.')
+
+  fig, ax = plt.subplots(figsize=(6, 6))
+  img = ax.imshow(frames[0], vmin=0, vmax=1)
+
+  def _update(frame):
+    img.set_data(frame)
+    return (img,)
+
+  interval = anim_secs * 1000 / len(frames)
+  anim = FuncAnimation(fig, _update, frames=frames, blit=True, repeat=True,
+                       interval=interval, repeat_delay=end_secs * 1000)
+  anim
+  plt.show()
 
 
 @webfn('Solve', '',
